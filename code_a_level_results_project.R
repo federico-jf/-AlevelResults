d723b75c77b49847051ae042bac576c5349ec45f
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2020-09-21 14:22:20 -0500

    Create code_a_level_results_project.R

---
+++ code_a_level_results_project.R
@@ -0,0 +1,14 @@
+#EPPS6356 / Prof. Dr. Karl Ho
+#Project Data Visualization
+#Student: Federico Ferrero
+#9/6/2020
+
+# Clear environment 
+rm(list=ls())
+
+# reading in the data
+mydata <- read.delim("/Users/feder/Desktop/t weets 9 6 2020.txt")
+nodes <- read.delim("/Users/feder/Desktop/nodes.txt")
+edges <- read.delim("/Users/feder/Desktop/edges.txt")
+
+#in progress...
